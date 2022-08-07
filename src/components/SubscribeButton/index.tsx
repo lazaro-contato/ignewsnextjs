67b8bb651f39a01c7464abf817e9503557c3b26e
José Lázaro <contato.jlazaro@gmail.com>
2022-08-06 22:16:32 -0300

    conect stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,37 @@
 import styles from './styles.module.scss'
+import {signIn, useSession} from "next-auth/react";
+import {api} from "../../services/api";
+import {getStripeJs} from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
 const SubscribeButton = ({priceId}: SubscribeButtonProps) => {
+  const {data: session} = useSession()
+
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout(sessionId)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   return(
-    <button type='button' className={styles.subscribeButton}>Subscribe Now</button>
+    <button onClick={() => handleSubscribe()} type='button' className={styles.subscribeButton}>Subscribe Now</button>
   )
 }
 
